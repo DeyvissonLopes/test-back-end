fae529915c2ef02046b8384c0e825fa5d8efb2e9
Deyvisson Lopes Alves <deyvissonlopes@outlook.com>
2020-10-17 18:01:10 -0300

    adição de um usuário específico via seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,5 +16,6 @@ class DatabaseSeeder extends Seeder
         \App\Models\User::factory(10)->create();
 
         $this->call(ProductTableSeeder::class);
+        $this->call(UserTableSeeder::class);
     }
 }
